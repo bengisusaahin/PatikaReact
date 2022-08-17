701a4d3b6a359b4acc8847b4131ff3d3c527c28e
Bengisu Şahin <bengisusaahin@gmail.com>
2022-08-17 15:57:18 +0300

    Added the array and used map and key properties.

--- Components/hello-react/src/App.js
+++ Components/hello-react/src/App.js
@@ -1,10 +1,31 @@
 import "./App.css";
 import User from "./components/User";
 
+const friends = [
+  {
+    id: 1,
+    name: "Ahmet",
+  },
+  {
+    id: 2,
+    name: "Tayfun",
+  },
+  {
+    id: 3,
+    name: "Ahmet2",
+  },
+];
+
 function App() {
   return (
     <>
-      <User name="Bengisu" surname="Sahin" isLoggedIn={true} age={23} />
+      <User
+        name="Bengisu"
+        surname="Sahin"
+        isLoggedIn={true}
+        age={23}
+        friends={friends}
+      />
     </>
   );
 }
