f8f1a54ad1ecce83f2a8bfdcf48581c071738b92
Bengisu Şahin <bengisusaahin@gmail.com>
2022-08-17 15:18:41 +0300

    Completed section

--- Components/hello-react/src/App.js
+++ Components/hello-react/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+
+const name = "Bengisu";
+const surname = "Şahin";
+const isLoggedIn = false;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <h1>{isLoggedIn && `Benim adım ${name},soyadım ${surname}`}</h1>
+
+      {!isLoggedIn && "Giriş yapmadınız."} */}
+
+      <h1>
+        {isLoggedIn
+          ? `Benim adım ${name},soyadım ${surname}`
+          : "Giriş yapmadınız."}
+      </h1>
+    </>
+    // <div>
+    //   <h1>Hello</h1>
+    //   <Header />
+    // </div>
   );
 }
 
---
+++ Components/hello-react/src/components/Header.js
@@ -0,0 +1,4 @@
+function Header() {
+  return <div>Merhaba ben Header Componentiyim</div>;
+}
+export default Header;
