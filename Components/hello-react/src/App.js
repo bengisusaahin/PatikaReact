32db3d3fc76ec5dad912434cf718e9f741877c68
Bengisu Şahin <bengisusaahin@gmail.com>
2022-08-17 15:45:08 +0300

    Started props section and added the User component

--- Components/hello-react/src/App.js
+++ Components/hello-react/src/App.js
@@ -1,27 +1,11 @@
 import "./App.css";
-import Header from "./components/Header";
-
-const name = "Bengisu";
-const surname = "Şahin";
-const isLoggedIn = false;
+import User from "./components/User";
 
 function App() {
   return (
     <>
-      {/* <h1>{isLoggedIn && `Benim adım ${name},soyadım ${surname}`}</h1>
-
-      {!isLoggedIn && "Giriş yapmadınız."} */}
-
-      <h1>
-        {isLoggedIn
-          ? `Benim adım ${name},soyadım ${surname}`
-          : "Giriş yapmadınız."}
-      </h1>
+      <User name="Bengisu" surname="Sahin" isLoggedIn={true} age={23} />
     </>
-    // <div>
-    //   <h1>Hello</h1>
-    //   <Header />
-    // </div>
   );
 }
 
