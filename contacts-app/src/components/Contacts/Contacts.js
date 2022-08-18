9e57c4a516b81b956e3591203922bf080818c5b2
Bengisu Şahin <bengisusaahin@gmail.com>
2022-08-18 17:38:33 +0300

    Added filtering and showing list

--- contacts-app/src/components/Contacts/Contacts.js
+++ contacts-app/src/components/Contacts/Contacts.js
@@ -3,15 +3,29 @@ import List from "./List/List";
 import Form from "./Form/Form";
 
 const Contacts = () => {
-  const [contacts, setContacts] = useState([]);
-
+  const [contacts, setContacts] = useState([
+    {
+      fullname: "Bengisu",
+      phone_number: "123123",
+    },
+    {
+      fullname: "Bengisu",
+      phone_number: "123123",
+    },
+    {
+      fullname: "Bengisu",
+      phone_number: "123123",
+    },
+  ]);
+  //list componentinden eklenen kisileri goruntuleyebilmek icin state i
+  //burda olusturup parametre olarak form componentine gonderdik
   useEffect(() => {
     console.log(contacts);
   }, [contacts]);
 
   return (
     <div>
-      <List />
+      <List contacts={contacts} />
       <Form addContact={setContacts} contacts={contacts} />
     </div>
   );
--- contacts-app/src/components/Contacts/List/List.js
+++ contacts-app/src/components/Contacts/List/List.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
-const List = () => {
-  return <div>Contact List</div>;
+const List = ({ contacts }) => {
+  const [filterText, setFilterText] = useState("");
+
+  const filtered = contacts.filter((item) => {
+    return Object.keys(item).some((key) =>
+      item[key]
+        .toString()
+        .toLowerCase()
+        .includes(filterText.toLocaleLowerCase())
+    );
+  });
+  return (
+    <div>
+      <input
+        placeholder="Filter contacts"
+        value={filterText}
+        onChange={(e) => setFilterText(e.target.value)}
+      />
+
+      <ul>
+        {filtered.map((contact, i) => (
+          <li key={i}>{contact.fullname}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default List;
